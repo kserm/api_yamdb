8fa2bc691939d9dd906fd80bf148849e915c3213
Владислав Кальендо <kaliendos@yahoo.com>
2022-08-28 16:31:11 +0300

    fix permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,6 +22,6 @@ class IsAuthorModeratorAdminOrReadOnly(BasePermission):
             request.method in SAFE_METHODS
             or request.method == 'POST' and request.user.is_authenticated
             or obj.author == request.user
-            or request.user.is_admin
-            or request.user.is_moderator
+            or request.user.role == "admin"
+            or request.user.role == "moderator"
         )
