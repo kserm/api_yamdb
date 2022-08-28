9bb6b3f16f699340c636f1a9dde39ec50321b1e9
Konstantin Ermachkov <kermachkov@gmail.com>
2022-08-28 15:06:54 +0300

    categories, genres and titles done and merged with master

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-<<<<<<< HEAD
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,17 +10,4 @@ urlpatterns = [
         name="redoc"
     ),
     path("api/", include("api.urls")),
-=======
-
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("redoc/",
-         TemplateView.as_view(template_name="redoc.html"),
-        name="redoc"
-    ),
-    path("api/", include("api.urls"))
-
-
->>>>>>> origin/master
 ]
