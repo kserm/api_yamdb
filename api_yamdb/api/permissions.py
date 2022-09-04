d84448f25ff6cbc675532a73baefa33bb65fdd04
Konstantin Ermachkov <kermachkov@gmail.com>
2022-09-04 20:05:29 +0300

    permissions fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,10 +3,9 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return(request.user.is_authenticated
-               and request.user.role == "admin"
-               or request.user.is_staff)
-
+        return (request.user.is_authenticated
+                and request.user.role == "admin"
+                or request.user.is_staff)
 
 
 class IsAdminOrReadOnly(BasePermission):
